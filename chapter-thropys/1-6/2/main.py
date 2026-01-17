2d722a02051dd7643ce1462646bad47fce93a1dd
Kevin Castaneda <pollopower74@gmail.com>
2026-01-17 17:29:22 -0600

    Added the second challenge from chapter 6

---
+++ chapter-thropys/1-6/2/main.py
@@ -0,0 +1,10 @@
+# Creating the inputs that will store two floaters
+print("Type the first float number")
+num1 = float(input())
+print ("Type the second float number")
+num2 = float(input())
+
+# Arithmetic operation
+result = num1 / num2
+
+print(result)
