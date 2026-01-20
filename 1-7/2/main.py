6f477186ad8b3e2e5eaf9992c2262d3ce9248eba
Kevin Castaneda <pollopower74@gmail.com>
2026-01-20 10:48:54 -0600

    Added chapter 7 - subchapters from 3 up to 4

---
+++ 1-7/2/main.py
@@ -0,0 +1,10 @@
+#1
+welcome_screen = str("Bill Split Calculator")
+print(f'{welcome_screen}')
+
+#2
+
+#print(f'Please. Type your billing amount:')
+bill_amount = float(input())
+#print(f'Please. Type the tip that you will spare:')
+tip_percentaje = float(input())
---
+++ 1-7/3/main.py
@@ -0,0 +1,15 @@
+#1
+welcome_screen = str("Bill Split Calculator")
+print(f'{welcome_screen}')
+
+#2
+
+#print(f'Please. Type your billing amount:')
+bill_amount = float(input())
+#print(f'Please. Type the tip that you will spare:')
+tip_percentaje = float(input())
+
+#3
+tip_amount = float(tip_percentaje / 100) * bill_amount
+total_amount = bill_amount + tip_amount
+print(f'{total_amount}')
