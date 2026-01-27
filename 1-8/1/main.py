8f6ca69d7ff2974921a7a7d81bde6536b252d3df
Kevin Castaneda <pollopower74@gmail.com>
2026-01-26 20:43:58 -0600

    Added Chapter 8 - Subchapter 1 including the challenge from it

---
+++ 1-8/1/main.py
@@ -0,0 +1,41 @@
+"""
+# Question/Answer 0
+for i in range (0,5):
+    print(f'The value of i is equal to {i}')
+    for j in range (5):
+        print(f'The value of J is equal to {j}')
+"""
+
+"""
+# Question/Answer 1
+text = "Capitulo-8"
+for chart in text:
+    print(f'The current chart value is: {chart}')
+print(f"Lenght of text: {len(text)}")
+"""
+
+"""
+# Question/Answer 2
+for i in range(3):
+    print(f"The current value of i: {i}")
+    i+= 2
+    print(f"The current value after the operation is: {i}")
+"""
+
+"""
+# Question/Answer 3
+for i in range(0, 10):
+    print(f"The number is: {i}")
+"""    
+
+"""
+# Question/Answer 4
+for i in range(3, 8):
+    print("Hello")
+"""
+
+"""
+# # Question/Answer 5
+for i in range(2, 6):
+    print(i)
+"""
---
+++ 1-8/challenges/main.py
@@ -0,0 +1,5 @@
+
+#This is the challenge #1 of the loop section:
+
+for i in range(3,28):
+    print(f"Hello Coddy: {i}")
