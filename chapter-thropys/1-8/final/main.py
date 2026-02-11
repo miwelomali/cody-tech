9f8d22cebaf7d56727ef084ba5257936c2f566fc
Kevin Castaneda <pollopower74@gmail.com>
2026-02-11 15:26:50 -0600

    Added final challenge of chapter 8

---
+++ chapter-thropys/1-8/final/main.py
@@ -0,0 +1,9 @@
+#print("Enter the amount of numbers to sum")
+n1 = int(input())
+result = 0
+for i in range (n1):
+    #print(f"Enter a number")
+    nd = int(input())
+    result += nd
+#print(f"The sum of the {n1} numbers is {result}")
+print(result)
