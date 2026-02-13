18587c0229bbf4bff1668c724c38ce9cf3dd5e86
Kevin Dino <dino@Kevins-MacBook-Air.local>
2026-02-13 16:21:37 -0600

    Added chapter 9 : challenge 1

---
+++ chapter-thropys/1-9/main.py
@@ -0,0 +1,11 @@
+# Declare the function print_large_number below
+n = int(input())
+large_number = int(50005000)
+
+# function
+def print_large_number():
+    print(f"{large_number}")
+
+for i in range(n):
+    # Call the function here
+    print_large_number()
