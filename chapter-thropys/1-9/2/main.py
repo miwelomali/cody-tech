344cb93bd6140ff4a287d72d5c609da56cdef399
Kevin Dino <dino@Kevins-MacBook-Air.local>
2026-02-13 16:50:51 -0600

    Added chapter 9 : challenge 2

---
+++ chapter-thropys/1-9/2/main.py
@@ -0,0 +1,9 @@
+# Declare the variables
+a = int(input())
+b = int(input())
+# Declare your function here
+def product (a,b):
+    result = a * b 
+    return result
+# Call your function here with the arguments a and b
+print(f"{product(a,b)}")
