8d3eea4697ff8a18afd601d6c5c40c7bc52ed94b
Kevin Dino <dino@Kevins-MacBook-Air.local>
2026-02-13 17:34:47 -0600

    Added Chapter 9 : Subchapter 3 challenge

---
+++ chapter-thropys/1-9/3/main.py
@@ -0,0 +1,10 @@
+# Creation of the input variable
+input_num = int(input())
+# Creation of the funtion
+def square_number (input_num):
+    result = (input_num * input_num)
+    return result
+# Creation of the variable within the final final_result
+final_result = square_number(input_num)
+# Print of the result
+print (f"{final_result}")
