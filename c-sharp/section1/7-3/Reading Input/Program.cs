a9a50627219bcf050ab98dfe315df26f0e9057d1
dino-kevin <pollopower74@gmail.com>
2026-07-27 19:57:10 -0600

    add/C# lecture 7-3

---
+++ c-sharp/section1/7-3/Reading Input/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Read the user's name
+        string userName = Console.ReadLine();
+
+        // Print the greeting message
+        if (userName != "")
+        {
+            Console.WriteLine($" Hello, {userName}");
+        }
+    }
+}
