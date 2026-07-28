d4b40d5da4a7ac2b31515ee212c9cd2f5815bfee
dino-kevin <pollopower74@gmail.com>
2026-07-27 20:07:47 -0600

    add/C# lecture 7-4

---
+++ c-sharp/section1/7-4/Parse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string firstNumberString = Console.ReadLine();
+        string secondNumberString = Console.ReadLine();
+
+        // Use Parse to convert the numbers
+        int firstNumber = int.Parse(firstNumberString);
+        int secondNumber = int.Parse(secondNumberString);
+
+        // Print the sum of the numbers
+        int result = firstNumber + secondNumber;  
+        Console.WriteLine(result);
+        
+    }
+}
