e2bc658b864e38139a0fd5aa66fefe22702da483
dino-kevin <pollopower74@gmail.com>
2026-07-27 20:17:44 -0600

    add/C# lecture 7-5

---
+++ c-sharp/section1/7-5/Till 120/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //get user age by parsing the string age from the console readline
+        int userAge = int.Parse(Console.ReadLine());
+        string finalAge = $"{until_120(userAge)} years till 120";
+        Console.WriteLine(finalAge);
+
+
+    }
+    public static int until_120(int userAge)
+    {
+        int ageAgainst = 120 - userAge;
+        return ageAgainst;
+    }
+}
