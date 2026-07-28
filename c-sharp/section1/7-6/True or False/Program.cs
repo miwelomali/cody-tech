18030a6dad079ad7b7aeb70d53e59f09d480b70a
dino-kevin <pollopower74@gmail.com>
2026-07-27 20:25:10 -0600

    add/C# lecture 7-6

---
+++ c-sharp/section1/7-6/True or False/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program {
+    public static void Main(string[] args) {
+        int inputUser = int.Parse(Console.ReadLine());
+        Console.WriteLine(checkInputUser(inputUser));
+
+    }
+    public static string checkInputUser(int inputUser)
+    {
+        if (inputUser == 1)
+        {
+            return "T";
+        }
+
+        else
+        {
+            return "F";
+        }
+
+    }
+}
