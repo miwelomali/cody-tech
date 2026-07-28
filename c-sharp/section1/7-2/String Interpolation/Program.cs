16b8701892f416a2d10f25e81a61f5d6853af839
dino-kevin <pollopower74@gmail.com>
2026-07-27 19:42:37 -0600

    add/C# From lecture 7-1 up to 7-2

---
+++ c-sharp/section1/7-2/String Interpolation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string rnd = Console.ReadLine();
+        if (rnd != "")
+        {
+            Console.WriteLine($"The input is: {rnd}");
+        }
+
+    }
+}
